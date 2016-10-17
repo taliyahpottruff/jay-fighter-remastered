27ebe253c578a73fe7999203d973883c0823aa99
TrentonPottruff2 <jayitinc@gmail.com>
2016-10-17 18:58:05 -0400

    Health Script

---
+++ Assets/Health.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+    private int health = 100;
+    private int maxHeath = 100;
+
+    public int GetHealth() {
+        return health;    
+    }
+}
