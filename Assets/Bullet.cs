bacf5f153c142361a93c0dda84f1b09d0c082fb5
Trenton Pottruff <jayitinc@gmail.com>
2016-10-18 02:33:46 -0400

    Bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour {
+    private Vector2 velocityOnAwake = Vector2.zero;
+
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = velocityOnAwake;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Destroy(this.gameObject);
+    }
+
+    public void SetVelocity(Vector2 newVelocity) {
+        rb.velocity = newVelocity;
+    }
+
+    public void SetVelocityOnAwake(Vector2 velocity) {
+        velocityOnAwake = velocity;
+    }
+}
---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerCombat : MonoBehaviour {
+    private Vector2 fireVector = Vector2.zero;
+    private Rigidbody2D rb;
+
+    private GameObject bulletPrefab;
+
+    private void Start() {
+        bulletPrefab = Resources.Load<GameObject>("Prefabs/Bullet");
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        fireVector = new Vector2(Input.GetAxis("FireHorizontal"), Input.GetAxis("FireVertical"));
+
+        if (fireVector != Vector2.zero) {
+            Vector2 direction = fireVector.normalized;
+            GameObject bulletObj = Instantiate(bulletPrefab, this.transform.position + (Vector3)direction, Quaternion.identity) as GameObject;
+            Bullet bullet = bulletObj.GetComponent<Bullet>();
+            bullet.SetVelocityOnAwake(rb.velocity + (direction * 10));
+        }
+    }
+}
