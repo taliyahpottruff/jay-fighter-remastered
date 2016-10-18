ceafe24ffa6605328669cab776ac661bd326f5e7
TrentonPottruff2 <jayitinc@gmail.com>
2016-10-17 21:13:02 -0400

    Enemies Follow

---
+++ Assets/CPU.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class CPU : MonoBehaviour {
+    public float speed = 3;
+
+    private Rigidbody2D rb;
+
+	private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+
+        speed = Random.Range(2f, 4.5f);
+	}
+	
+	void Update() {
+        Vector2 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector2 targetDirection = (playerPosition - (Vector2) transform.position).normalized;
+        rb.velocity = targetDirection * speed;
+	}
+}
---
+++ Assets/GameCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+    public Transform player;
+    public Transform map;
+
+    private Vector2 mapSize = Vector2.zero;
+
+    private void Update() {
+        mapSize = new Vector2(map.localScale.x / 2, map.localScale.y / 2);
+
+        float xRatio = player.position.x / mapSize.x;
+        float yRatio = player.position.y / mapSize.y;
+
+        Vector3 cameraVector = new Vector3((mapSize.x / 2) * xRatio, (mapSize.y / 2) * yRatio, -10);
+        Camera.main.transform.position = cameraVector;
+    }
+}
