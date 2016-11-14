b5e7a9f0e0ad45214c7d0d4204dbb2130fc8c0e2
FireGamer3 <gm31334@gmail.com>
2016-11-14 01:12:01 -0600

    Started Work on a continuing gameplay

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,32 @@ public class GameManager : MonoBehaviour {
     private GameObject playerPrefab;
     private GameObject player;
     public static long Score = 0;
+    public static int Round = 1;
     private Health Health;
+    private GameObject[] spawned;
+    public GameObject[] spawnPoints = new GameObject[4];
+
+    private GameObject basicEnemy;
+    private GameObject fastEnemy;
+    private GameObject shootEnemy;
+    private GameObject dupeEnemy;
 
     public GameObject SCORE;
     public GameObject HEALTH;
+    public GameObject ROUND;
 
     private Text scoreText;
     private Text healthText;
-	void Start () {
+    private Text roundText;
+    void Start () {
+        basicEnemy = Resources.Load<GameObject>("Prefabs/Enemies/Enemy");
+        fastEnemy = Resources.Load<GameObject>("Prefabs/Enemies/Fast Enemy");
+        shootEnemy = Resources.Load<GameObject>("Prefabs/Enemies/Shooting Enemy");
+        dupeEnemy = Resources.Load<GameObject>("Prefabs/Enemies/Duplicator Enemy");
+
         scoreText = SCORE.GetComponent<Text>();
         healthText = HEALTH.GetComponent<Text>();
+        roundText = ROUND.GetComponent<Text>();
         playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
         player = (GameObject)Instantiate(playerPrefab, new Vector3(0,0,0), Quaternion.identity);
         Health = player.GetComponent<Health>();
@@ -30,5 +46,28 @@ public class GameManager : MonoBehaviour {
         if(healthText.text != Health.GetHealth().ToString()) {
             healthText.text = Health.GetHealth().ToString();
         }
+        if(roundText.text != Round.ToString()) {
+            roundText.text = Round.ToString();
+        }
 	}
+    //Selects a spawnpoint (this returns an int to be used with the array)
+    private int selectSP() {
+        return (int)Mathf.Round(Random.Range(0f, 3.1f));
+    }
+    //private GameObject getEnemy() {
+    //    if(Round < 5) {
+    //        return basicEnemy;
+    //    } else {
+    //        float ran = Mathf.Round(Random.Range(0f, 3.1f));
+    //        if(Round >= 20) {
+    //
+    //        }else if(Round >= 15) {
+    //
+    //        }else if(Round >= 10) {
+    //
+    //        } else {
+    //
+    //        }
+    //    }
+    //}
 }
