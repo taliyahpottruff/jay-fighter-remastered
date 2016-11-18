3f6be0e4e1a139e1d8c5b8cbefea74e8622b3a91
Trenton Pottruff <jayitinc@gmail.com>
2016-11-18 01:46:08 -0500

    Add Object Panel

---
+++ Assets/Scripts/AddObjectButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddObjectButton : MonoBehaviour {
+    public string objectToSpawn;
+
+    public void SpawnObject() {
+        Vector2 spawnLocation = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width/2, Screen.height/2));
+        spawnLocation = new Vector2((int)spawnLocation.x, (int)spawnLocation.y);
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/MapEditorObjects/" + objectToSpawn);
+        GameObject go = Instantiate(prefab, spawnLocation, Quaternion.identity) as GameObject;
+        go.name = prefab.name;
+    }
+}
