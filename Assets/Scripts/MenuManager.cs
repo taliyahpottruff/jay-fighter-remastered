66e39ef8eb34e77539534062f239004337fc7d67
Trenton Pottruff <jayitinc@gmail.com>
2016-12-06 02:33:07 -0500

    Basic Main Menu
    Exit Button still needs to be done.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject[] menus;
+
+    public void ChangeMenu(int menuIndex) {
+        for (int i = 0; i < menus.Length; i++) {
+            if (i == menuIndex) {
+                menus[i].SetActive(true);
+            } else {
+                menus[i].SetActive(false);
+            }
+        }
+    }
+}
