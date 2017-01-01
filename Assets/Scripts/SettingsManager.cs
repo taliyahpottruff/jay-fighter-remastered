7cdf335edbf72460f2fc7c5f441152e5efe262b7
Trenton Pottruff <jayitinc@gmail.com>
2017-01-01 02:09:31 -0500

    Ability to manually set resolution

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SettingsManager : MonoBehaviour {
+    private void Start() {
+        GameObject.Find("Fullscreen Toggle").GetComponent<Toggle>().isOn = Screen.fullScreen;
+    }
+
+    public void SetFullscreen(bool fullscreen) {
+        Screen.fullScreen = fullscreen;
+    }
+
+    public void SetResolutionWidth(string size) {
+        Screen.SetResolution(int.Parse(size), Screen.height, Screen.fullScreen);
+    }
+
+    public void SetResolutionHeight(string size) {
+        Screen.SetResolution(Screen.width, int.Parse(size), Screen.fullScreen);
+    }
+}
