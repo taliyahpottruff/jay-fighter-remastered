b3c24a17e397f6193cbc417b544e887301742383
Trenton Pottruff <jayitinc@gmail.com>
2017-02-18 14:49:36 -0500

    Smooth Camera - Smoothness

---
+++ Assets/SmoothCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+    private Transform player;
+
+    private void Update() {
+        if (player == null) {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            return;
+        }
+
+        Vector3 newPosition = player.position;
+        newPosition.z = -10;
+        this.transform.position = newPosition;
+    }
+}
