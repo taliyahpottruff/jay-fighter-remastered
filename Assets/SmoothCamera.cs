d1426ce253ddf08b4aea79ec5730ff1b7ebd2874
Trenton Pottruff <jayitinc@gmail.com>
2017-02-18 15:11:12 -0500

    Smooth Camera

--- Assets/SmoothCamera.cs
+++ Assets/SmoothCamera.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmoothCamera : MonoBehaviour {
-    private Transform player;
+    public Transform lookAt;
 
-    private void Update() {
-        if (player == null) {
-            player = GameObject.FindGameObjectWithTag("Player").transform;
+    private float smoothSpeed = 5f;
+    private Vector3 offset = new Vector3(0, 0, -10);
+
+    public void FixedUpdate() {
+        if (lookAt == null) {
+            Debug.Log("lookAt is null!");
+            lookAt = GameObject.FindGameObjectWithTag("Player").transform;
             return;
         }
 
-        Vector3 newPosition = player.position;
-        newPosition.z = -10;
-        this.transform.position = newPosition;
+        Vector3 newPosition = lookAt.position + offset;
+
+        //Smooth
+        transform.position -= (transform.position - newPosition) * smoothSpeed * Time.deltaTime;
     }
 }
