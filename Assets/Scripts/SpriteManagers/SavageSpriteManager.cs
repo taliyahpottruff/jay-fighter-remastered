8a61f863081af048646b7880f6991df0029f10d1
Trenton Pottruff <jayitinc@gmail.com>
2017-03-31 14:56:39 -0400

    Organization

---
+++ Assets/Scripts/SpriteManagers/SavageSpriteManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavageSpriteManager : EnemySpriteManager {
+    public override void MoveDown() {
+        throw new NotImplementedException();
+    }
+
+    public override void MoveLeft() {
+        throw new NotImplementedException();
+    }
+
+    public override void MoveRight() {
+        throw new NotImplementedException();
+    }
+
+    public override void MoveUp() {
+        throw new NotImplementedException();
+    }
+}
