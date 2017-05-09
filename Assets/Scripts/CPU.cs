214ac8a7d65673fdcc6e7fb4e0b23280de87f98a
Trenton Pottruff <jayitinc@gmail.com>
2017-05-09 13:58:56 -0400

    Bullets Fire on Network

--- Assets/Scripts/CPU.cs
+++ Assets/Scripts/CPU.cs
@@ -197,7 +197,7 @@ public class CPU : NetworkBehaviour {
                 Vector2 direction = td.normalized;
                 GameObject bulletObj = Instantiate(bulletPrefab, this.transform.position + (Vector3)direction, Quaternion.identity) as GameObject;
                 Bullet bullet = bulletObj.GetComponent<Bullet>();
-                bullet.owner = this.transform;
+                bullet.owner = GetComponent<NetworkIdentity>();
                 bullet.damage = 5;
                 bullet.SetVelocityOnAwake(rb.velocity + (direction * 10));
             }
