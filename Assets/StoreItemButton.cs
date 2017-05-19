64f59b8f8c75fd9eef410a178a468f2e0212c3da
Trenton Pottruff <jayitinc@gmail.com>
2017-05-19 10:39:50 -0400

    Fixed stores

--- Assets/StoreItemButton.cs
+++ Assets/StoreItemButton.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class StoreItemButton : MonoBehaviour {
     public string itemName;
     public int cost;
 
     private Text button;
+    private Player player;
     private Inventory playerInventory;
 
     private void Start() {
@@ -17,7 +19,9 @@ public class StoreItemButton : MonoBehaviour {
     private IEnumerator DelayedStart() {
         yield return new WaitForSeconds(0.11f);
         button = GetComponentInChildren<Text>();
-        playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        GameObject playerObj = NetworkManager.singleton.client.connection.playerControllers[0].gameObject;
+        player = playerObj.GetComponent<Player>();
+        playerInventory = playerObj.GetComponent<Inventory>();
     }
 
     private void Update() {
@@ -25,8 +29,8 @@ public class StoreItemButton : MonoBehaviour {
     }
 
     public void BuyItem() {
-        if (GameManager.Coins < cost) return;
-        GameManager.Coins -= cost;
+        if (player.coins < cost) return;
+        player.coins -= cost;
 
         //Loop through inventory to see if item already exists
         for (int i = 0; i < playerInventory.inventory.Count; i++) {
