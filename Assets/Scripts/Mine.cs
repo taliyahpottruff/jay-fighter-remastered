751881f856eaf0b9f5b11f79203a372761676145
Trenton Pottruff <jayitinc@gmail.com>
2017-06-27 21:34:21 -0400

    Mines

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Mine : MonoBehaviour {
+    [SerializeField]
+    private GameObject explosionPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Entity")) {
+            Instantiate<GameObject>(explosionPrefab, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
