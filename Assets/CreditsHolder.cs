4fac8ba8637ef0a94e98b967cf7b889c7fe63b3e
Trenton Pottruff <jayitinc@gmail.com>
2017-08-01 00:32:04 -0400

    Credits

---
+++ Assets/CreditsHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsHolder : MonoBehaviour {
+    public MenuManager menuManager;
+
+    public void BackToMain() {
+        StartCoroutine(AnimTimer());
+    }
+
+    IEnumerator AnimTimer() {
+        yield return new WaitForSeconds(60f);
+        menuManager.ChangeMenu(0);
+    }
+}
